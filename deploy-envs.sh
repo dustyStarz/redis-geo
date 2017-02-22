6d0ba57bbc8ab4f6cb82e724dd3306b5cedb93b6
dackerman <dusty.ackerman@starz.com>
2017-02-22 08:49:10 -0700

    updated virtual host

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -6,7 +6,8 @@ export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=us-west-2
 export AWS_ECS_CLUSTER_NAME=default
-export AWS_VIRTUAL_HOST=redisgeo.netcore.io
+#export AWS_VIRTUAL_HOST=redisgeo.netcore.io
+export AWS_VIRTUAL_HOST=ec2-35-161-97-151.us-west-2.compute.amazonaws.com
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
